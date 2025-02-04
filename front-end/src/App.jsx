6be1b8fc7982bfc2204330d73cf5a2f1763e6e38
shabaz khan <135216873+Shabazkhan1352@users.noreply.github.com>
2025-02-04 12:20:07 +0530

    CORS ADDED
    login function created

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,18 +1,40 @@
-import { useState } from 'react'
-import Login from './components/Login'
-import Results from './components/Results'
-import Voting from './components/Voting'
-import './App.css'
+import { useState, useEffect } from "react";
+import Login from "./components/Login";
+import Results from "./components/Results";
+import Voting from "./components/Voting";
+import "./App.css";
 
 function App() {
-  
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    // Check if the user has a valid token
+    const token = localStorage.getItem("token");
+    if (token) {
+      setIsAuthenticated(true);
+    }
+  }, []);
 
   return (
     <div>
-      <Login />
-      
+      {isAuthenticated ? (
+        <>
+          <Voting />
+          <Results />
+          <button
+            onClick={() => {
+              localStorage.removeItem("token"); // Logout
+              setIsAuthenticated(false);
+            }}
+          >
+            Logout
+          </button>
+        </>
+      ) : (
+        <Login setIsAuthenticated={setIsAuthenticated} />
+      )}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
