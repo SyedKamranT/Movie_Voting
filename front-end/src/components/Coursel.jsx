30ec46b99912cb8d1da9df481e11f1cc409b4f45
Syed Kamran <98963964+SyedKamranT@users.noreply.github.com>
2025-02-10 10:48:58 +0530

    coursal

--- front-end/src/components/Coursel.jsx
+++ front-end/src/components/Coursel.jsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
 import corosal1 from "../assets/corosal.png";
-import corosal2 from "../assets/corosal.png";
-import corosal3 from "../assets/corosal.png";
+import corosal2 from "../assets/corosal2.png";
+import corosal3 from "../assets/corosal3.png";
+import corosal4 from "../assets/corosal4.png";
 
 const Carousel = () => {
 
